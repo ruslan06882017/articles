b75a266e69274dd6e2c5de779d3d4b1f0990a75f
Ruslan Gusseinov <r.gusseinov@mail.ru>
2020-05-09 23:43:20 +0600

    Adde React select

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ArticleList from './components/article-list'
 import articles from './fixtures'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Select from 'react-select'
 
 
-function App() {
-  return (
-    <ArticleList articles={articles} />
-  );
+class App extends Component {
+  state = {
+    openItem: null
+  }
+  render(){
+    return (
+      <div>
+        <Select options={this.options} value={this.state.openItem} onChange={this.handleSelect} />
+        <ArticleList articles={articles} />
+      </div>
+    )
+  }
+
+  get options(){
+    return articles.map(article => ({
+      label: article.title,
+      value: article.id
+    }))
+  }
+
+  handleSelect = openItem => this.setState({ openItem });
 }
 
 export default App;
