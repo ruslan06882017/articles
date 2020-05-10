2a64991207e4430f428f4ee05a5972cf1abc8c2e
Ruslan Gusseinov <r.gusseinov@mail.ru>
2020-05-10 14:10:06 +0600

    Added Date picket component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,69 @@ import articles from './fixtures'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Select from 'react-select'
 import UserForm from './components/user-form'
+import DayPicker, { DateUtils } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+import styles from './styles.css'
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.handleResetClick = this.handleResetClick.bind(this);
+    this.state = this.getInitialState();
+  }
+
   state = {
     openItem: null
   }
+  static defaultProps = {
+    numberOfMonths: 2,
+  };
+  getInitialState() {
+    return {
+      from: undefined,
+      to: undefined,
+    };
+  }
+
+  handleDayClick(day) {
+    const range = DateUtils.addDayToRange(day, this.state);
+    this.setState(range);
+  }
+
+  handleResetClick() {
+    this.setState(this.getInitialState());
+  }
+
   render(){
+    const { from, to } = this.state;
+    const modifiers = { start: from, end: to };
     return (
       <div>
+      <div className="RangeExample">
+        <p>
+          {!from && !to && 'Please select the first day.'}
+          {from && !to && 'Please select the last day.'}
+          {from &&
+            to &&
+            `Selected from ${from.toLocaleDateString()} to
+                ${to.toLocaleDateString()}`}{' '}
+          {from && to && (
+            <button className="link" onClick={this.handleResetClick}>
+              Reset
+            </button>
+          )}
+        </p>
+        <DayPicker
+          className="Selectable"
+          numberOfMonths={this.props.numberOfMonths}
+          selectedDays={[from, { from, to }]}
+          modifiers={modifiers}
+          onDayClick={this.handleDayClick}
+        />
+        </div>
         <UserForm />
         <Select 
           options={this.options}
